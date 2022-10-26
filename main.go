3b7e7d8753bcadbf41cfb974d8409981f7466cf4
kave haj <kave.haj@snapp.cab>
2022-10-27 00:10:16 +0330

    append movie lits

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"fmt"
+	"log"
+	"net/http"
 
 	"github.com/gorilla/mux"
 )
@@ -22,8 +25,37 @@ var movies []Movie
 func main() {
 	r := mux.NewRouter()
 
+	movies = append(movies,
+		Movie{
+			ID: "1",
+			Isbn: "438227",
+			Title: "Movie 1",
+			Director: &Director{
+				Firstname: "John",
+				Lastname: "Doe",
+			},
+		})
+
+	movies = append(movies,
+		Movie{
+			ID: "2",
+			Isbn: "45455",
+			Title: "Movie 2",
+			Director: &Director{
+				Firstname: "Stieve",
+				Lastname: "Smith",
+			},
+
+		})
+
+
 	r.HandleFunc("/movies", getMovies).Methods("GET")
 	r.HandleFunc("/movies/{id}", getMovie).Methods("GET")
-	r.HandleFunc("movies",createMovie).Methods("POST")
+	r.HandleFunc("movies", createMovie).Methods("POST")
+	r.HandleFunc("movies/{id}", updateMovie).Methods("PUT")
+	r.HandleFunc("movies/{id}", deleteMovie).Methods("DELETE")
 
+	fmt.Printf("starting server at port 8000 \n")
+
+	log.Fatal(http.ListenAndServe("8000", r))
 }
