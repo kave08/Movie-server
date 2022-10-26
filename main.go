45e6175fe2f547fabf23dbd6bd7c9878c49c930e
kave haj <kave.haj@snapp.cab>
2022-10-26 23:42:50 +0330

    Start Project :)

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+
+	"github.com/gorilla/mux"
+)
+
+type Movie struct {
+	ID       string    `json:"id"`
+	Isbn     string    `json:"isbn"`
+	Title    string    `json:"title"`
+	Director *Director `json:"director`
+}
+
+type Director struct {
+	Firstname string `json:"firstname"`
+	Lastname  string `json:"lastname"`
+}
+
+var movies []Movie
+
+func main() {
+	r := mux.NewRouter()
+
+	r.HandleFunc("/movies", getMovies).Methods("GET")
+	r.HandleFunc("/movies/{id}", getMovie).Methods("GET")
+	r.HandleFunc("movies",createMovie).Methods("POST")
+
+}
